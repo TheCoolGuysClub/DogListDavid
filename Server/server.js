eab370f0e4d0e00f91b33f968888f833ae401974
DavidWei95036593 <dw36593@pausd.us>
2018-04-11 13:35:00 -0700

    why is it not working

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,12 @@
 const express = require(`express`);
 const bodyParser = require(`body-parser`);
+const mongoose = require(`mongoose`);
 const {Dog}=require(`./models/dog.js`);
 const app = express();
 const port = process.env.PORT||3000;
 
+mongoose.Promise = global.Promise;
+const database = process.env.MONGODB_URI||`mogodb://localhost:27017`;
 
 app.get(`/`,(req,res)=>{
   res.send(`GET /`);
