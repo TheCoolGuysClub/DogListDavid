c48d071c2a7b4929605ee30b2a4430b86de1c87e
DavidWei95036593 <dw36593@pausd.us>
2018-04-11 13:44:56 -0700

    still not working

--- Server/server.js
+++ Server/server.js
@@ -3,17 +3,34 @@ const bodyParser = require(`body-parser`);
 const mongoose = require(`mongoose`);
 const {Dog}=require(`./models/dog.js`);
 const app = express();
+//-------------------------------set up----------------------------------
 const port = process.env.PORT||3000;
 
 mongoose.Promise = global.Promise;
 const database = process.env.MONGODB_URI||`mogodb://localhost:27017`;
-
+mongoose.connect(database)
+.then(()=>{
+  console.log(`connected to database`);
+}).catch(()=>{
+  console.log(`unable to connect to database`);
+})
+//-------------------------------set up----------------------------------
 app.get(`/`,(req,res)=>{
   res.send(`GET /`);
 })
 
 app.post(`/dogs`,(req,res)=>{
-  res.send(`POST /dogs`);
+  const dog = new Dog({
+    name:`Spot`,
+    age:2
+  })
+  dog.save()
+    .then(dog=>{
+      res.send(dog);
+    })
+    .catch(error=>{
+      res.status(400).send();
+    })
 })
 
 app.listen(port,()=>{
